4ce79be10b30e1eeeaa6b8659bd0d71d5ff1eadb
jota <jjotaa0one@gmail.com>
2025-12-16 09:34:38 -0300

    Delete src directory

--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("menu");
-const trigger = document.getElementById("menu-trigger");
-
-// Abrir/fechar menu
-trigger.addEventListener("click", () => {
-    const aberto = menu.dataset.aberto === "true";
-    menu.dataset.aberto = !aberto;
-});
